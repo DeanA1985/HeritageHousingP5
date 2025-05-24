4148e4ebe875eb6f63dd71172227a0b7c6e067da
Dean <designersuite@outlook.com>
2025-05-24 21:17:27 +0100

    Add about page with project overview and author info

---
+++ dashboard/pages/3_About.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+st.header("ℹ️ About This Project")
+
+st.markdown("""
+This dashboard is part of a **Code Institute Final Project**. "
+It allows users to:"
+
+
+- Estimate housing prices using trained machine learning models.
+- Explore insights from historical sale data.
+- Download prediction reports for future reference.
+
+**Technologies Used:** Streamlit, Scikit-learn, Plotly, Pandas.
+
+**Author:** [DeanA1985](https://github.com/DeanA1985/HeritageHousingP5)
+
+---
+""")
+
+# Footer
+st.markdown("""
+<center>
+Made with ❤️ for the Code Institute Final Project<br>
+© 2025 Heritage Housing Estimator
+</center>
+""", unsafe_allow_html=True)
